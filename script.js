351701a38fb22e6cf9f88c9c15b6202aea2dbfeb
Sajal1005 <nandasajal.208@gmail.com>
2023-08-28 16:18:28 +0530

    done

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ if(document.readyState == "loading"){
   ready();
 }
 
+const cartList = [];
 
 function ready(){
   //Remove Items form Cart
@@ -43,10 +44,16 @@ function ready(){
   }
 }
 
+
 //Remove Items from Cart
 function removeCartItem(event){
+
   var buttonClicked = event.target;
-  buttonClicked.parentElement.parentElement.remove();
+  buttonClicked.parentElement.remove();
+  var cartItemsNames = document.getElementsByClassName("cart-product-title");
+  document.querySelector('#cart-num').innerHTML=cartItemsNames.length;
+
+  console.log(cartItemsNames)
   updatetotal();
 }
 
@@ -75,23 +82,25 @@ function addProductToCart(title, price, productImg){
   cartShopBox.classList.add("cart-box");
   var cartItems = document.getElementsByClassName('cart-content')[0];
   var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+  document.querySelector('#cart-num').innerHTML=cartItemsNames.length+1;
   for (var i=0; i < cartItemsNames.length; i++) {
-      if (cartItemsNames[i].innerText == title) {
-          alert("You have already added this item in your cart"); 
-          return;
-      }  
+    // console.log( cartItemsNames)
+
+    //   if (cartItemsNames[i].innerHTML.localeCompare(title)) {
+    //       alert("You have already added this item in your cart"); 
+    //       return;
+    //   }  
   }
   var cartBoxContent = `
                   
-                  <img height="100px" class="cart-img" src="${productImg}" alt="">
-                  <div class="detail-box">
-                      <div class="cart-product-title">${title}</div>
-                      <div class="cart-price">${price}</div>
-                      <input type="number" value="1" class="cart-quantity"> 
-                  </div>
-                  <svg class="cart-remove" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="red" class="w-6 h-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                  </svg>  
+  <img height="100px" class="cart-img" src="${productImg}" alt="">
+  <div class="detail-box">
+      <div class="cart-product-title">${title}</div>
+      <div class="cart-price">${price}</div>
+      <input type="number" value="1" class="cart-quantity"> 
+  </div>
+  <box-icon class="cart-remove" name='trash' type='solid' color='#ff0000' ></box-icon>
+                  
                                  `;
 
                 cartShopBox.innerHTML = cartBoxContent;
@@ -133,3 +142,5 @@ function addProductToCart(title, price, productImg){
                 document.getElementsByClassName("total-price")[0].innerText = "$" + total;
                 
             }
+
+            
